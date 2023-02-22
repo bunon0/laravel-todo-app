f264efdc7e84ad0c15703646a463fd3f12e5ae89
bunon0 <86172592+bunon0@users.noreply.github.com>
2023-02-22 12:16:39 +0900

    ✨ 編集と削除モーダルの追加

--- resources/views/goals/index.blade.php
+++ resources/views/goals/index.blade.php
@@ -21,5 +21,40 @@
                 </div>
             </a>
         </div>
+
+        <div class="row row-cols-1 row row-cols-md-2 row-cols-lg-3 g-4">
+            @foreach ($goals as $goal)
+                @include('modals.edit-goal')
+                @include('modals.delete-goal')
+
+                <div class="col">
+                    <div class="card bg-light">
+                        <div class="card-body d-flex justify-content-between align-items-center">
+                            <h4 class="card-title ms-1 mb-0">{{ $goal->title }}</h4>
+                            <div class="d-flex align-items-center">
+                                <div class="dropdown">
+                                    <a href="#"
+                                        class="dropdown-toggle px-1 fs-5 fw-bold link-dark text-decoration-none"
+                                        id="dropdownGoalMenuLink" role="button" data-bs-toggle="dropdown"
+                                        aria-expanded="false">︙</a>
+                                    <ul class="dropdown-menu dropdown-menu-end text-center"
+                                        aria-labelledby="dropdownGoalMenuLink">
+                                        <li>
+                                            <a href="#" class="dropdown-item" data-bs-toggle="modal"
+                                                data-bs-target="#editGoalModal{{ $goal->id }}">編集</a>
+                                        </li>
+                                        <div class="dropdown-divider"></div>
+                                        <li>
+                                            <a href="#" class="dropdown-item" data-bs-toggle="modal"
+                                                data-bs-target="#deleteGoalModal{{ $goal->id }}">削除</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
 @endsection
